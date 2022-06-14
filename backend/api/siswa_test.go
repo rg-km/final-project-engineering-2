b6530818a860644e8fce062fa9e10e2335f0ce56
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-06-15 00:06:46 +0700

    PWB-21 test : add testing for if theres no siswa yet

--- backend/api/siswa_test.go
+++ backend/api/siswa_test.go
@@ -44,6 +44,62 @@ var _ = Describe("Siswa testing", func() {
 
 		})
 
+		It("Should Return Empty list", func() {
+			db, err := sql.Open("sqlite3", "./beasiswa.db")
+			Expect(err).To(BeNil())
+
+			db.Exec(`CREATE TABLE IF NOT EXISTS beasiswa (
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
+				nama TEXT,
+				jenis_beasiswa TEXT,
+				jenjang_pendidikan TEXT,
+				tanggal_mulai TEXT,
+				tanggal_selesai TEXT);
+				
+				CREATE TABLE IF NOT EXISTS siswa (
+					id INTEGER PRIMARY KEY AUTOINCREMENT,
+					nama TEXT,
+					password TEXT,
+					email TEXT,
+					jenjang_pendidikan TEXT,
+					nik TEXT,
+					tanggal_lahir TEXT,
+					tempat_lahir TEXT);
+				
+				CREATE TABLE IF NOT EXISTS pendaftaran (
+					id INTEGER PRIMARY KEY AUTOINCREMENT,
+					id_beasiswa INTEGER,
+					id_siswa INTEGER,
+					tanggal_daftar TEXT,
+					status TEXT);
+				
+				CREATE TABLE IF NOT EXISTS mitra (
+					id INTEGER PRIMARY KEY AUTOINCREMENT,
+					nama TEXT,
+					email TEXT,
+					lokasi TEXT,
+					no_telp TEXT,
+					legalitas TEXT);`)
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest("GET", "/api/siswa/all", nil)
+			siswaRepo := repository.NewSiswaRepository(db)
+			api := NewApi(*siswaRepo)
+			api.GetAllSiswa(w, r)
+
+			res := w.Result()
+			defer res.Body.Close()
+			body, err := ioutil.ReadAll(res.Body)
+			Expect(err).To(BeNil())
+
+			var data []Siswa
+
+			err = json.Unmarshal(body, &data)
+			Expect(err).To(BeNil())
+
+			Expect(len(data)).To(Equal(0))
+
+		})
+
 	})
 
 })
