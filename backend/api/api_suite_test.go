40ada67d752d29982c56f473c1e9ac50ab2df2b2
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-06-14 14:17:16 +0700

    PWB-21 test : add testing for get all siswa

---
+++ backend/api/api_suite_test.go
@@ -0,0 +1,12 @@
+package api
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestApi(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Api Suite")
+}
---
+++ backend/api/siswa_test.go
@@ -0,0 +1,57 @@
+package api
+
+import (
+	"database/sql"
+	"encoding/json"
+	"final-project-eng2-be/db"
+	"final-project-eng2-be/repository"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"io/ioutil"
+	"net/http/httptest"
+	"os"
+)
+
+var _ = Describe("Siswa testing", func() {
+
+	BeforeEach(func() {
+		db, err := sql.Open("sqlite3", "./beasiswa.db")
+		if err != nil {
+			panic(err)
+		}
+		database.Migrate(db)
+	})
+
+	AfterEach(func() {
+		os.Remove("./beasiswa.db")
+	})
+
+	Describe("Siswa Data", func() {
+
+		It("Should Return all Siswa", func() {
+			db, err := sql.Open("sqlite3", "./beasiswa.db")
+			Expect(err).To(BeNil())
+
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest("GET", "/api/siswa/all", nil)
+			siswaRepo := repository.NewSiswaRepository(db)
+			api := NewApi(*siswaRepo)
+			api.GetAllSiswa(w, r)
+
+			res := w.Result()
+			defer res.Body.Close()
+			body, err := ioutil.ReadAll(res.Body)
+			Expect(err).To(BeNil())
+
+			var data []Siswa
+
+			err = json.Unmarshal(body, &data)
+			Expect(err).To(BeNil())
+
+			Expect(len(data)).To(Equal(2))
+
+		})
+
+	})
+
+})
