b9b09f49e3e8437f9881c0b368b9a36d9031dc98
HAKN1999 <hafizdkurniawan700@gmail.com>
2022-06-14 16:15:28 +0700

    PWB-14 feat: add repo beasiswa and create get all beasiswa endpoint

--- backend/api/api.go
+++ backend/api/api.go
@@ -1,39 +1,40 @@
 package api
 
 import (
-	"final-project-eng2-be/repository"
 	"fmt"
 	"net/http"
+
+	"final-project-eng2-be/repository"
 )
 
 type API struct {
-	mux *http.ServeMux
-	siswaRepo repository.SiswaRepository
+	mux          *http.ServeMux
+	siswaRepo    repository.SiswaRepository
+	beasiswaRepo repository.BeasiswaRepository
 }
 
-func NewApi(siswaRepo repository.SiswaRepository) *API {
+func NewApi(siswaRepo repository.SiswaRepository, beasiswaRepo repository.BeasiswaRepository) *API {
 	mux := http.NewServeMux()
 
 	api := &API{
-		mux: mux,
-		siswaRepo: siswaRepo,
+		mux:          mux,
+		siswaRepo:    siswaRepo,
+		beasiswaRepo: beasiswaRepo,
 	}
 
 	mux.Handle("/api/login", api.POST(http.HandlerFunc(api.login)))
 	mux.Handle("/api/register", api.POST(http.HandlerFunc(api.register)))
 
+	mux.Handle("/api/beasiswa/all", api.GET(http.HandlerFunc(api.getBeasiswa)))
+
 	return api
 }
 
-func (api *API) Handler() *http.ServeMux{
+func (api *API) Handler() *http.ServeMux {
 	return api.mux
 }
 
-func (api *API) Start(){
+func (api *API) Start() {
 	fmt.Println("Server is running on port 8080")
 	http.ListenAndServe(":8080", api.Handler())
 }
-
-
-
-
---
+++ backend/api/beasiswa.go
@@ -0,0 +1,54 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+)
+
+type BeasiswaListErrorResponse struct {
+	Error string `json:"error"`
+}
+
+type ListBeasiswa struct {
+	Id                string `json:"id"`
+	Nama              string `json:"nama"`
+	JenisBeasiswa     string `json:"jenis_beasiswa"`
+	JenjangPendidikan string `json:"jenjang_pendidikan"`
+	TanggalMulai      string `json:"tanggal_mulai"`
+	TanggalSelesai    string `json:"tanggal_selesai"`
+}
+
+type BeasiswaListSuccessResponse struct {
+	Beasiswa []ListBeasiswa `json:"beasiswa"`
+}
+
+func (a *API) getBeasiswa(w http.ResponseWriter, r *http.Request) {
+	encoder := json.NewEncoder(w)
+	w.Header().Set("Content-Type", "application/json")
+	response := BeasiswaListSuccessResponse{}
+	response.Beasiswa = make([]ListBeasiswa, 0)
+
+	beasiswa, err := a.beasiswaRepo.GetAll()
+	defer func() {
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			encoder.Encode(BeasiswaListErrorResponse{Error: err.Error()})
+		}
+	}()
+	if err != nil {
+		return
+	}
+	for _, b := range beasiswa {
+		response.Beasiswa = append(response.Beasiswa, ListBeasiswa{
+			Id:                strconv.Itoa(int(b.Id)),
+			Nama:              b.Nama,
+			JenisBeasiswa:     b.JenisBeasiswa,
+			JenjangPendidikan: b.JenjangPendidikan,
+			TanggalMulai:      b.TanggalMulai,
+			TanggalSelesai:    b.TanggalSelesai,
+		})
+	}
+
+	encoder.Encode(response)
+}
